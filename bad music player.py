5b4a1ecd9419e1e40ac53733d41aa6c1247fdf63
CarrionAndOn <maya@carrionandon.me>
2023-03-28 15:37:33 -0700

    bad music player.py WAV support (without metadata though)

--- bad music player.py
+++ bad music player.py
@@ -8,6 +8,7 @@ base_folder = "I:/Music"
 
 # ask the user what song they want
 song_name = input("Enter a song name: ").lower()
+# ask what artist in case of songs that have the same name as eachother
 artist_name = input("Enter the artist's name: ").lower()
 
 # search the folders for the requested song and artist
@@ -23,6 +24,7 @@ if matching_files:
     track_name = None
     artist_name = None
     album_name = None
+    year = None
 
     # check if the file is a FLAC or MP3 and use the appropriate method to get the metadata
     if matching_files[0].endswith('.flac'):
@@ -30,14 +32,25 @@ if matching_files:
         track_name = audio['title'][0]
         artist_name = audio['artist'][0]
         album_name = audio['album'][0]
+        year = audio['date'][0]
     elif matching_files[0].endswith('.mp3'):
         audio = EasyID3(matching_files[0])
         track_name = audio['title'][0]
         artist_name = audio['artist'][0]
         album_name = audio['album'][0]
+        year = audio['date'][0]
+    # WAV file metadata is far too inconsistent to be able to check it, so skip metadata if it's a wav
+    elif matching_files[0].endswith('.wav'):
+        print("WAV file, method to get metadata is too inconsistent, skipping metadata check!")
 
     # print the track information
-    print(f"Playing {track_name} by {artist_name} from {album_name}")
+    if matching_files[0].endswith('.flac'):
+        print(f"Playing {track_name} by {artist_name} from {album_name} ({year})")
+    elif matching_files[0].endswith('.mp3'):
+        print(f"Playing {track_name} by {artist_name} from {album_name} ({year})")
+    # if it is a wav, just show the full filename as a compromise since the metadata is too inconsistent to grab
+    elif matching_files[0].endswith('.wav'):
+        print(f"Playing {matching_files}")
 
     # play the song
     pygame.init()
